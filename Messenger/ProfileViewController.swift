56569e838b6c26962163a922990a73e3ce442e75
Bogdan <bbelotserkovtsev@gmail.com>
2021-02-21 23:15:54 +0300

    implement avatar changing

--- Messenger/ProfileViewController.swift
+++ Messenger/ProfileViewController.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
-	@IBOutlet weak var profilePicture: UIView?
-	@IBOutlet weak var nameAndLastname: UILabel?
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	
+	@IBOutlet weak var profilePictureView: UIImageView?
+	@IBOutlet weak var nameAndLastnameLabel: UILabel?
 	@IBOutlet weak var editButton: UIButton?
+	@IBOutlet weak var initialsLabel: UILabel?
+	
+	private var profilePicture: UIImage? {
+		willSet {
+			if let img = newValue {
+				profilePictureView?.image = img
+				initialsLabel?.isHidden = true
+			} else {
+				profilePictureView?.image = nil
+				initialsLabel?.isHidden = false
+			}
+		}
+	}
 	
 	required init?(coder: NSCoder) {
 		super.init(coder: coder)
@@ -21,13 +34,61 @@ class ProfileViewController: UIViewController {
 //		на этом этапе вьюшки еще не созданы
 	}
 	
+	//MARK: Image Picker
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			profilePicture = image
+		}
+		dismiss(animated: true, completion: nil)
+	}
+	
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		dismiss(animated: true, completion:nil)
+	}
+	
+	func presentImagePicker(_ picker: UIImagePickerController ,with sourceType: UIImagePickerController.SourceType) {
+		picker.sourceType = sourceType
+		self.present(picker, animated: true, completion: nil)
+	}
+	
+	//MARK: Gestures
+	@objc func handleProfilePictureTap(_ sender: UITapGestureRecognizer) {
+		let alert = UIAlertController(title: "Set profile picture", message: nil, preferredStyle: .actionSheet)
+		
+		let imagePicker = UIImagePickerController()
+		imagePicker.allowsEditing = false
+		imagePicker.delegate = self
+
+		alert.addAction(UIAlertAction(title: "Library", style: .default, handler: { _ in
+			if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+				self.presentImagePicker(imagePicker, with: .photoLibrary)
+			}
+			
+		}))
+		
+		alert.addAction(UIAlertAction(title: "Take picture", style: .default, handler: { _ in
+			if UIImagePickerController.isSourceTypeAvailable(.camera) {
+				self.presentImagePicker(imagePicker, with: .camera)
+			}
+		}))
+		
+		alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		
+		self.present(alert, animated: true, completion: nil)
+	}
+	
+	//MARK: Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		print("In \(#function): ", editButton?.frame ?? "nil")
 		
-		profilePicture?.layer.cornerRadius = 240 / 2
+		profilePictureView?.layer.cornerRadius = 240 / 2
 		editButton?.layer.cornerRadius = 14
 		editButton?.isEnabled = false
+		
+		let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfilePictureTap(_:)))
+		profilePictureView?.addGestureRecognizer(tap)
+		profilePictureView?.isUserInteractionEnabled = true
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
